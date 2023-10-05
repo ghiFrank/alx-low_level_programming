68678c32c805c916d94cc91f196642511d03d0f1
frank <ayoub3412@gmail.com>
2023-10-05 05:32:08 -0700

    y

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void) 
+{
+	 printf("Size of char: %zu byte(s)\n", sizeof(char));
+	 printf("Size of int: %zu byte(s)\n", sizeof(int));
+	 printf("Size of long: %zu byte(s)\n", sizeof(long));
+	 printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	 printf("Size of float: %zu byte(s)\n", sizeof(float));
+    return 0;
+}
