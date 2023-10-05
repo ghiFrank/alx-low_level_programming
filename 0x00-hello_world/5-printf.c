ff290ab9df8465ae51b4b303d800d220c3f6d824
frank <ayoub3412@gmail.com>
2023-10-05 05:20:46 -0700

    y

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - main function
  *
- * Retrun: returns 0
+ * Return: returns 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
