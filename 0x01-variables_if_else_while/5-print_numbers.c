2342b7b4257415b8997ae97b5f6c41458fc71b4b
frank <ayoub3412@gmail.com>
2023-10-06 09:13:52 -0700

    y

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);   
+	}
+	return (0);
+}
