c4743835f6d37b97ad8909561b9ec53a0698b8d3
frank <ayoub3412@gmail.com>
2023-10-06 08:41:42 -0700

    y

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
