5173093d423544e296128847ac85e09a30848f21
frank <ayoub3412@gmail.com>
2023-10-10 12:58:42 -0700

    y

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,12 @@ int main(void)
 	int i;
 
 	int total = 0;
+	
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			total = total + i;
 	}
-       	printf("%d\n",total);
+	printf("%d\n", total);
 	return (0);
 }
