bd333d38d3943f776657efac122ebabeeb6f00ed
frank <ayoub3412@gmail.com>
2023-10-10 11:56:59 -0700

    y

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,77 +1,38 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * print_to_98 - prints from n to 98
  * @n: starting number
  */
 void print_to_98(int n)
 {
-		if (n <= 98)
+	if (n <= 98)
+	{
+		for (;n <= 98; n++)
 		{
-				for (; n <= 98; n++)
-				{
-						if (n < 0)
-						{
-								n = -n;
-								_putchar('-');
-								if (n < 9)
-								{
-										_putchar('0' + n);
-										_putchar(',');
-										_putchar('	');
-								}
-								else
-								{
-										_putchar('0' + n / 10);
-										_putchar('0' + n % 10);
-										_putchar(',');
-										_putchar('	');
-								}
-								n = -n;
-						}
-						else
-						{
-								if (n < 9)
-								{
-										_putchar('0' + n);
-										_putchar(',');
-										_putchar('	');
-								}
-								else
-								{
-										_putchar('0' + n / 10);
-										_putchar('0' + n % 10);
-										if (n != 98)
-										{
-												_putchar(',');
-												_putchar('	');
-										}
-								}
-						}
-				}
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		else
-		{
-				for (; n >= 98; n--)
-				{
-						if (n > 99)
-						{
-								_putchar('0' + (n / 100));
-								_putchar('0' + (n % 100) / 10);
-								_putchar('0' + n % 10);
-								_putchar(',');
-								_putchar('	');
-						}
-						else
-						{
-								_putchar('0' + n / 10);
-								_putchar('0' + n % 10);
-								if (n != 98)
-								{
-										_putchar(',');
-										_putchar('	');
-								}
-						}
-				}
-		}
+	}
+	else
+	{
+		for (;n >= 98; n--)
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+	}
 }
