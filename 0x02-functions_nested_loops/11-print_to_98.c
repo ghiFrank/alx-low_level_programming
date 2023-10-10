7add9a4439a946c9fb98f38c2e62622f688960a9
frank <ayoub3412@gmail.com>
2023-10-10 11:41:04 -0700

    y

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,47 +4,82 @@
  * print_to_98 - prints from n to 98
  * @n: starting number
  */
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
-	{
-		if (n < 0)
+		if (n <= 98)
 		{
-			n = -n;
-			_putchar('-');
-			if (n < 9)
-			{
-				_putchar('0' + n);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('0' + n / 10);
-				_putchar('0' + n % 10);
-				_putchar(',');
-				_putchar(' ');
-			}
-			n = -n;
+				for (; n <= 98; n++)
+				{
+						if (n < 0)
+						{
+								n = -n;
+								_putchar('-');
+								if (n < 9)
+								{
+										_putchar('0' + n);
+										_putchar(',');
+										_putchar('	');
+								}
+								else
+								{
+										_putchar('0' + n / 10);
+										_putchar('0' + n % 10);
+										_putchar(',');
+										_putchar('	');
+								}
+								n = -n;
+						}
+						else
+						{
+								if (n < 9)
+								{
+										_putchar('0' + n);
+										_putchar(',');
+										_putchar('	');
+								}
+								else
+								{
+										_putchar('0' + n / 10);
+										_putchar('0' + n % 10);
+										if (n != 98)
+										{
+												_putchar(',');
+												_putchar('	');
+										}
+								}
+						}
+				}
 		}
 		else
 		{
-			if (n < 9)
-			{
-				_putchar('0' + n);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('0' + n / 10);
-				_putchar('0' + n % 10);
-				if (n != 98)
+				for (; n >= 98; n--)
 				{
-					_putchar(',');
-					_putchar(' ');
+						if (n > 99)
+						{
+								_putchar('0' + (n / 100));
+								_putchar('0' + (n % 100) / 10);
+								_putchar('0' + n % 10);
+								_putchar(',');
+								_putchar('	');
+						}
+						else
+						{
+								_putchar('0' + n / 10);
+								_putchar('0' + n % 10);
+								if (n != 98)
+								{
+										_putchar(',');
+										_putchar('	');
+								}
+						}
 				}
-			}
 		}
-	}
 }
