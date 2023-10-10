0975c4bb3a6317a2068b983a0fb8d6fcf69dce95
frank <ayoub3412@gmail.com>
2023-10-10 08:33:07 -0700

    y

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/*
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char n;
+		for (n = 'a';n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
