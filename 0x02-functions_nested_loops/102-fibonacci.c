1b53f967ae6dddcf2613c226666e49ed441c31a7
frank <ayoub3412@gmail.com>
2023-10-10 13:15:10 -0700

    y

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	int pc1 = 1;
+	int pc2 = 2;
+	int pc3;
+	printf("1, 2, ");
+	for (i = 3; i <= 48; i++)
+	{
+		pc3 = pc1 + pc2;
+		printf("%d, ", pc3);
+		pc1 = pc2;
+		pc2 = pc3;
+	}
+	printf("\n");
+	return (0);
+}
