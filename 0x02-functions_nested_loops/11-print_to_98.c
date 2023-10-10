9b5ad8376bb61aa33458ce05cc3442f0a52e20a1
frank <ayoub3412@gmail.com>
2023-10-10 11:18:58 -0700

    y

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,42 @@ void print_to_98(int n)
 	for (; n <= 98; n++)
 	{
 		if (n < 0)
-			_putchar('-');
-		if (n < 9)
 		{
-			_putchar('0' + abs(n));
-			_putchar(',');
-			_putchar(' ');
+			n = -n;
+			_putchar('-');
+			if (n < 9)
+			{
+				_putchar('0' + n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+				_putchar(',');
+				_putchar(' ');
+			}
+			n = -n;
 		}
 		else
 		{
-			_putchar('0' + abs(n % 10));
-			_putchar('0' + abs(n / 10));
-			_putchar(',');
-			_putchar(' ');
+			if (n < 9)
+			{
+				_putchar('0' + n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+				if (n != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
 	}
 }
