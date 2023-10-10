25e223170c76e833f0f8bb2e616b33ae698c3bcd
frank <ayoub3412@gmail.com>
2023-10-10 13:22:30 -0700

    y

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int i;
-	long long pc1 = 1;
-	long long pc2 = 2;
-	long long pc3;
+	long pc1 = 1;
+	long pc2 = 2;
+	long pc3;
 
 	printf("1, 2, ");
 	for (i = 3; i <= 50; i++)
 	{
 		pc3 = pc1 + pc2;
-		printf("%lld, ", pc3);
+		printf("%ld, ", pc3);
 		pc1 = pc2;
 		pc2 = pc3;
 	}
