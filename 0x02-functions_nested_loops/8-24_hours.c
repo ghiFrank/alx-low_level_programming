77f6a1f64c186493d61d93ca77cabd7882edfd4c
frank <ayoub3412@gmail.com>
2023-10-10 10:11:06 -0700

    y

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, int j;
+	
+	for (n = 0; n <= 23; n++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
+			_putchar(':');
+			_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+			_putchar('\n');
+		}
+	}
+}
