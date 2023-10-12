5d314391c1508c57ca3b78373b32481253248d5a
frank <ayoub3412@gmail.com>
2023-10-12 07:55:18 -0700

    y

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,11 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 61 && year % 2 == 0)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else if (month == 2 && day == 60 && year % 2 != 0)
+		if (month == 2 && day == 60 && year % 2 != 0)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
