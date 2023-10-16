830c31e1ab9c04e33b101e3718a786256c74f333
frank <ayoub3412@gmail.com>
2023-10-16 11:14:19 -0700

    y

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: buffer
+ * Return: returns the integer
+ */
+int _atoi(char *s)
+{
+	int i = atoi(s);
+
+	return (i);
+}
