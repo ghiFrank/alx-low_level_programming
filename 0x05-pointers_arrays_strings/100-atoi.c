c038bc9db54da61110dfb0371a02318b9111146e
frank <ayoub3412@gmail.com>
2023-10-16 11:33:30 -0700

    y

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,17 @@
  */
 int _atoi(char *s)
 {
-	int i = atoi(s);
+	int n = 0;
+	int sign = 1;
 
-	return (i);
+	for (; *s; s++)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	}
+	return (n * sign);
 }
