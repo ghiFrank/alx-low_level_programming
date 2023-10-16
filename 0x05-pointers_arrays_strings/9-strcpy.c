068e53ab0bc21a616073f1c8e96ac0a988892612
frank <ayoub3412@gmail.com>
2023-10-16 11:01:44 -0700

    y

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - copies the string pointed to by src copies the string pointed to by src
+ * @dest: buffer
+ * @src: string
+ * Return: returns string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
