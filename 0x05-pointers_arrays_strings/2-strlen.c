f4355ba7bb062c7908605c64d116ee1f43f54d2f
frank <ayoub3412@gmail.com>
2023-10-16 09:05:27 -0700

    y

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
