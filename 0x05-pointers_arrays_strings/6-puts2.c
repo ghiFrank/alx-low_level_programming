b8f8d98db66e8fd364f48f6be49e50987fcdb577
frank <ayoub3412@gmail.com>
2023-10-16 10:24:12 -0700

    y

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
 /**
- * puts2 - prints every other string
+ * puts_half - prints half of a string
  * @str: string
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int i = 0;
 	int len = strlen(str);
+	int i;
+
+	if (strlen(str) % 2 != 0)
+		i = ((len - 1) / 2) + 1;
+	else
+		i = len / 2;
 
 	for (; i < len; i++)
 	{
-		if (i % 2 == 0)
-		{
-			printf("%c", str[i]);
-		}
+		printf("%c", str[i]);
 	}
 	printf("\n");
 }
