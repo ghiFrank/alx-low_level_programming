5bbdca3ab5546b9f622039f54eea4b39c192795e
frank <ayoub3412@gmail.com>
2023-10-18 14:08:25 -0700

    y

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,24 +3,44 @@
 #include <string.h>
 #include <ctype.h>
 /**
+ *	is_sep - checks if c is a separator
+ *	@c: a character
+ *	Return: 1 if true, 0 if false
+ */
+int is_sep(char c)
+{
+	int i;
+	char arr[] = "	\t\n,;.!?\"(){}";
+
+	for (i = 0; i < 13; i++)
+	{
+		if (c == arr[i])
+			return (1);
+	}
+	return (0);
+}
+/**
  *	*cap_string - capitalizes all words of a string
  *	@n: a string
  *	Return: string with all words capitalized
  */
 char *cap_string(char *n)
 {
-	int i = 0;
-	char arr[] = " \n\t,;.!?\"(){}";
+	char *ptr = n;
+	int sep = 1;
 
-	for (; n[i] != '\0'; i++)
+	while (*n)
 	{
-		int j = 0;
-
-		for (; j < 13; j++)
+		if (is_sep(*n))
+			sep = 1;
+		else if (sep)
 		{
-			if (n[i] == arr[j])
-				n[i + 1] = toupper(n[i + 1]);
+			*n = toupper(*n);
+			sep = 0;
 		}
+		else
+			sep = 0;
+		n++;
 	}
-	return (n);
+	return (ptr);
 }
