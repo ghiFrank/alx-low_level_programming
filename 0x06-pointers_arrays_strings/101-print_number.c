14fb4e51857f04345993b4298c0bd4e70cc0584b
frank <ayoub3412@gmail.com>
2023-10-18 16:04:55 -0700

    y

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_number - prints an integer
+*@n: an integer
+*/
+void print_number(int n)
+{
+	int j;
+
+	j = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		j = -n;
+	}
+	if (j / 10 != 0)
+	{
+		print_number(j / 10);
+	}
+	_putchar('0' + (j % 10));
+}
