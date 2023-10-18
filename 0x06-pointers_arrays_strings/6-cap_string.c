be97a5ac6871c87462e880579159e9ceec699361
frank <ayoub3412@gmail.com>
2023-10-18 12:36:02 -0700

    y

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,17 @@
 char *cap_string(char *n)
 {
 	int i = 0;
-
+	char arr[10] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	for (; n[i] != '\0'; i++)
 	{
-		if (isspace(n[i]) || !isalnum(n[i]))
+		int j = 0;
+		if (isspace(n[i]) || n[i] == '\n')
 			n[i + 1] = toupper(n[i + 1]);
+		for (; j < 10; j++)
+		{
+			if (n[i] == arr[j])
+				n[i + 1] = toupper(n[i + 1]);
+		}
 	}
 	return (n);
 }
