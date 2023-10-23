ecac1586719cbf049024b5a1b3c52b3183ca71ba
frank <ayoub3412@gmail.com>
2023-10-23 05:06:10 -0700

    y

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_memcpy - fills memory with a constant byte.
+ * @src: first memory area
+ * @dest: second constant byte
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
