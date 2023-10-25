f41d5ab40fb9516c16ec4c04ae00ca778e8f23ab
frank <ayoub3412@gmail.com>
2023-10-25 00:09:29 -0700

    y

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * _puts_recursion - prints a string in reverse.
+ *_print_rev_recursion - prints a string in reverse.
  * @s: the string
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	int i = strlen(s) - 1;
 
