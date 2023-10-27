c0d2b1c9a4d9d5f14df8020a9d3287071ead397e
frank <ayoub3412@gmail.com>
2023-10-27 15:04:36 -0700

    y

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,8 @@
  * @argv: array of arguments
  * Return: returns 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argc;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
