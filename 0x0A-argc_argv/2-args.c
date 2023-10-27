b181d8f3a6524acb57b71eb82d7d267612244221
frank <ayoub3412@gmail.com>
2023-10-27 15:07:30 -0700

    y

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: total arguments
+ * @argv: array of arguments
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int n = 1;
+
+	for (; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
