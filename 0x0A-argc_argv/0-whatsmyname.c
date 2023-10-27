7a6a8591bfdd5ce224e1109a941135343b6744a8
frank <ayoub3412@gmail.com>
2023-10-27 14:51:30 -0700

    y

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ * main - main function
+ * @argc: total arguments
+ * @argv: array of arguments
+ * Return: returns 0
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
