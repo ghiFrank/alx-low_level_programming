2b27b65e8558abe65009eae752b5f7fd6a38a19a
frank <ayoub3412@gmail.com>
2023-10-30 01:26:26 -0700

    y

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * free_grid - frees grid
+ * @grid: grid of memories
+ * @height: array height
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
