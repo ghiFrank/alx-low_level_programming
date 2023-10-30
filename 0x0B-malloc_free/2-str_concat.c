8106a77a03dd85cd4f169899a10ca39062895f23
frank <ayoub3412@gmail.com>
2023-10-30 00:42:50 -0700

    y

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strdup - concatenates two strings
+ * _str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * Return: returns a pointer
