e34aef8d6345ccffe194891c503a96222aa5d84c
frank <ayoub3412@gmail.com>
2023-11-02 08:50:34 -0700

    y

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,11 +19,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	for (s1l = 0; s1[s1l] != '\0'; s1l++)
 		;
-        for (s2l = 0; s2[s2l] != '\0'; s2l++)
-                ;
+	for (s2l = 0; s2[s2l] != '\0'; s2l++)
+		;
 	arr = malloc(s1l + n + 1);
 	if (arr == NULL)
-		return(NULL);
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		arr[i] = s1[i];
 	for (j = 0; j < n; j++)
