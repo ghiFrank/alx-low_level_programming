a4ce9172506af65f95a2b0fbad8c59d08b9badbc
frank <ayoub3412@gmail.com>
2023-11-06 01:14:50 -0800

    y

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* dog */
