40e7508c6bcad877dccb05b89c85a0502603e1a9
frank <ayoub3412@gmail.com>
2023-11-06 02:42:05 -0800

    y

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,13 +9,24 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
-
 	if (!name || age < 0 || !owner)
 		return (NULL);
 	doggo = (dog_t *)malloc(sizeof(dog_t));
 	if (doggo == NULL)
 		return (NULL);
+	doggo->name = malloc(sizeof(char) * (strlen(name)));
+	if ((*doggo).name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+	doggo->owner = malloc(sizeof(char) * (strlen(owner)));
+	if ((*doggo).owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
 	doggo->name = name;
 	doggo->age = age;
 	doggo->owner = owner;
