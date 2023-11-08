dc68025371b847bdf417d7525029ae3b8878e46a
frank <ayoub3412@gmail.com>
2023-11-08 03:40:58 -0800

    y

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,18 +8,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if ( array && size < 0 && cmp)
+	if (array && size < 0 && cmp)
 	{
 		int n = 0;
 
-		if (size <= 0)
-			return (-1);
 		for (; n < size; n++)
 		{
 			if (cmp(array[n]))
 				return (n);
+			else
+				return (-1);
 		}
-		return (-1);
 	}
 	return (-1);
 }
