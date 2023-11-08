ebeb52f0f21ed8289e80c966f619c7ffe27c131d
frank <ayoub3412@gmail.com>
2023-11-08 04:42:51 -0800

    y

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: total args
+ * @argv: array of args
+ * Return : returns 0
+ */
+int main(int argc, char *argv[])
+{
+	char *ptr = (char *)main;
+	int n;
+
+	if (argc != 2)
+		printf("Error\n");
+		exit(1);
+	n = atoi(argv[1]);
+	if (n <0)
+		printf("Error\n");
+		exit(1);
+	while (n--)
+		printf("%02hhx%s", *ptr++, n ? " " : "\n");
+	return (0);
+}
