40ca186a971781f0de54bba4291d0deac3c4cf8e
frank <ayoub3412@gmail.com>
2023-11-08 04:10:56 -0800

    y

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,31 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /** main - main function
- * Return: returns int
+ * @argc: total args
+ * @argv: array of args
+ * Return: returns 0
  */
-int main(void)
+int main(int argc, char * argv[])
 {
 
+	if (argc != 4)
+	{
+		printf("Error");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error");
+		exit(99);
+	}
+	if ((argv[2] == "/" || argv[2] == "%") && atoi(argv[3]) == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	int num = (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	
+	printf("%d", num);
+	return (0);
+}
