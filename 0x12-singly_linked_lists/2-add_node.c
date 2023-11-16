88ef15ee60f81a7668d8dc995dc760eb605c798b
frank <ayoub3412@gmail.com>
2023-11-16 06:42:19 -0800

    y

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <string.h>
 /**
- * list_len - adds a new node at the beginning of a list_t list.
+ * add_node - adds a new node at the beginning of a list_t list.
  * @head: the list
  * @str: string to duplicate
  * Return: returns the address of the new element, or NULL if it failed
