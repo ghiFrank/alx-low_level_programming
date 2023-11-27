9a733b0c4877059cbb582b18d0c3bdea377366cf
frank <ayoub3412@gmail.com>
2023-11-27 03:50:49 -0800

    y

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (len)
-		bytes = write(fd, text_conent, len);
+		bytes = write(fd, text_content, len);
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }
