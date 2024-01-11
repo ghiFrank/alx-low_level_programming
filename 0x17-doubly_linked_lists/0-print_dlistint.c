ead64e479e1a041fc18d27c4974697b3bfa46eb1
frank <ayoub3412@gmail.com>
2024-01-11 10:56:28 -0800

    y

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,13 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t x = 0;
+	size_t x = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        x++;
-    }
-    return (x);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
 }
